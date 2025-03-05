d78119ada4c9fc1cb2545a21c88c84da20a6ec1b
Onizukachi <dark_sao@mail.ru>
2025-03-05 20:55:03 +0300

    lesson 1

---
+++ app/controllers/api/v1/admin/books_controller.rb
@@ -0,0 +1,14 @@
+module Api
+  module V1
+    module Admin
+      class BooksController < ApplicationController
+        # before_action :authenticate_admin_user!
+
+        def index
+          books = Book.page(params[:page] || 1).per(Settings.app.items_per_page)
+          render json: BookSerializer.new(books)
+        end
+      end
+    end
+  end
+end
