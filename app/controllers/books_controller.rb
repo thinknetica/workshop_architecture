7a76aff543fd4c524e5c0c6bb92f717fe24123bf
atiugaev <atiugaev@synergy.ru>
2025-03-09 18:10:05 +0300

    fix after review

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,17 @@
 class BooksController < ApplicationController
   def index
-    books =  Book.all.order(:created_at).limit(::Settings.app.items_per_page)
-    page = params[:page].to_i
-    books = books.offset(page * ::Settings.app.items_per_page) if page > 1
+    books =  Book.order(:created_at).limit(per_page).offset(page * per_page)
 
     render json: serialize(books)
+  end
+
+  private
+
+  def per_page
+    ::Settings.app.items_per_page
+  end
+
+  def page
+    [ params[:page].to_i, 0 ].max
   end
 end
